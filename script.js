a6b01a959660049eaf8d777d0014db9235e72c5a
Rohit Kumawat <117036470+rohitk7879@users.noreply.github.com>
2023-04-18 13:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-//your code here
+//your code he
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+function removeArticle(str){
+	let words = str.split(' ');	
+	if(words.length > 1 && (words[0].toLowerCase()==='a'
+							|| words[0].toLowerCase()==='an'
+							|| words[0].toLowerCase()==='the'
+						   )
+		){
+		words.splice(0,1);
+	}
+	return words.join(" ");
+}
+touristSpots.sort((a,b)=>{
+	const bandA = removeArticle(a);
+	const bandB = removeArticle(b);
+	return bandA.localeCompare(bandB);
+})
+let ul = document.getElementById("band");
+touristSpots.forEach(band => {
+	let li = document.createElement('li');
+	li.innerText = band;
+	ul.appendChild(li);
+});
 
